2d78b66743b80ab4ef3c14fe4cfd2f5095284d2c
HevilynOliveira <hevilynoliveira205@gmail.com>
2024-12-05 09:51:31 -0300

    Aula-2

---
+++ AULA-2/aula02.py
@@ -0,0 +1,10 @@
+print('Bom dia!')
+nome_usuario = 'Hevilyn Oliveira' # tipo -> str = string
+idade_usuario=  18 # tipo -> int = integer 
+altura_usuario= 1.56 # tipo -> flloat = ponto flutuante 
+maior_idade = True # tipo -> bool = boolean
+
+print('Seja Bem-vindo' , nome_usuario),
+print('sua idade é' , idade_usuario, 'anos,')
+print('voce tem' , altura_usuario, 'metros.')
+print('voce é maior de idade?', maior_idade) 
